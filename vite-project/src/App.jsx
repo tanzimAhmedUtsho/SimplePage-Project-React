d371eca0937c4cf26566b3da5285acc6c7bfae69
Tanzim Ahmed <utshotanzim@gmail.com>
2024-12-02 23:15:04 +0600

    added navbar

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,18 @@
+import './App.css'
 const App =()=>{
 return<div>
-My first component 
+<nav>
+    <div className="logo">
+<img src="../public/images/brand_logo.png" alt="logo" />
+    </div>
+    <ul>
+        <li href='#'>Menu</li>
+        <li href='#'>Location</li>
+        <li href='#'>About</li>
+        <li href='#'>Contact</li>
+    </ul>
+    <button>Login</button>
+</nav>
 </div>;
 };
 export default App;
