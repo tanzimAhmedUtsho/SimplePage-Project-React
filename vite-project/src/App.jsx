4f25cdb9476567be85ac56b96d36e366aa5b3fad
Tanzim Ahmed <utshotanzim@gmail.com>
2024-12-02 23:58:13 +0600

    content section added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,18 +1,10 @@
 import './App.css'
+import HeroSection from './components/Hero';
+import Navigation from './components/Navigation';
 const App =()=>{
 return<div>
-<nav>
-    <div className="logo">
-<img src="../public/images/brand_logo.png" alt="logo" />
-    </div>
-    <ul>
-        <li href='#'>Menu</li>
-        <li href='#'>Location</li>
-        <li href='#'>About</li>
-        <li href='#'>Contact</li>
-    </ul>
-    <button>Login</button>
-</nav>
+<Navigation/>
+<HeroSection/>
 </div>;
 };
 export default App;
---
+++ vite-project/src/components/Hero.jsx
@@ -0,0 +1,27 @@
+const HeroSection =()=>{
+    return <main className="hero">
+        <div className="hero-content">
+            <h1>YOUR FEET 
+            DESERVE THE BEST</h1>
+            <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+            <div className="hero-btn">
+                <button>Shop Now</button>
+                <button>Category</button>
+
+            </div>
+            <div className="shopping">
+             <p>Also Available On</p>
+             <div className="brand-icons">
+             <img src="../../public/images/amazon.png" alt="amazon-logo" />
+             <img src="../../public/images/flipkart.png" alt="flipkart-logo" />
+             </div>
+            </div>
+        </div>
+        <div className="hero-image"></div>
+<div className="hero-image">
+    <img src="../../public/images/shoe_image.png" alt="hero-image" />
+</div>
+    </main>
+};
+
+export default HeroSection
